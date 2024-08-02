79e94c67a4848a807260b03507e95448a4bca56e
zetaxbyte <zetaxlinux@gmail.com>
2024-08-02 16:52:38 +0800

    move all props to folder props

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -54,10 +54,10 @@ BOARD_DTBOIMG_PARTITION_SIZE := 25165824
 TARGET_BOARD_PLATFORM := trinket
 
 # Properties
-TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
-TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
-TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
-TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/props/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/props/vendor.prop
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/props/product.prop
+TARGET_ODM_PROP += $(DEVICE_PATH)/props/odm.prop
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
